20e21f0cec57434ad939c95fff07b21ae986b6e4
wefmaika1985@gmail.com <wefmaika1985@gmail.com>
2025-07-13 23:53:07 +0900

    nuxt3の環境を作成

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/fonts'],
+  app: {
+    baseURL: '/thex-score'
+  }
+})
