46707b7c17d7f5c02d99136b94f4f91b1bf9b625
wefmaika1985@gmail.com <wefmaika1985@gmail.com>
2025-07-19 03:08:28 +0900

    エラーページの作成

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+  <TheHeader/>
+  <UContainer class="py-8">
+    <h1 class="text-xl font-bold mb-6">エラーが発生しました</h1>
+    <p class="mb-6">{{ error?.statusCode }} - {{ error?.message }}</p>
+    <div class="flex justify-center">
+      <UButton to="/">トップページへ戻る</UButton>
+    </div>
+  </UContainer>
+  <TheFooter/>
+</template>
+
+<script setup>
+defineProps({
+  error: Object
+})
+</script>
