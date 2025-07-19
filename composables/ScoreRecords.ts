dae3111c5ef44f9538018c6dba2d38ca31df86e1
wefmaika1985@gmail.com <wefmaika1985@gmail.com>
2025-07-19 18:57:12 +0900

    記録を削った

--- composables/ScoreRecords.ts
+++ composables/ScoreRecords.ts
@@ -120,52 +120,6 @@ export function useScoreRecords(){
         }
       }
     },
-    'にゃんこ3': {
-      'th07': {
-        'ReimuA': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': null,
-          'detail': '-',
-        },
-        'ReimuB': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': null,
-          'detail': '-',
-        },
-        'MarisaA': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': 'th7_ud0002.rpy',
-          'detail': '-',
-        },
-        'MarisaB': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': null,
-          'detail': '-',
-        },
-        'SakuyaA': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': 'th7_ud0002.rpy',
-          'detail': '-',
-        },
-        'SakuyaB': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': null,
-          'detail': '-',
-        }
-      }
-    }
   }
 
   return scoreRecords
