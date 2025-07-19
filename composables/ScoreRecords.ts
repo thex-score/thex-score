70b0cf82fb4546e007fdbdccbab220ac8ab2caef
wefmaika1985@gmail.com <wefmaika1985@gmail.com>
2025-07-19 18:59:58 +0900

    記録に整合性を持たせた

--- composables/ScoreRecords.ts
+++ composables/ScoreRecords.ts
@@ -62,53 +62,7 @@ export function useScoreRecords(){
           'score': 13000000,
           'status': 'good',
           'date': '2024-03-11T15:30:00',
-          'replay': 'th7_ud0002.rpy',
-          'detail': '-',
-        },
-        'SakuyaB': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
           'replay': null,
-          'detail': '-',
-        }
-      }
-    },
-    'にゃんこ2': {
-      'th07': {
-        'ReimuA': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': null,
-          'detail': '-',
-        },
-        'ReimuB': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': null,
-          'detail': '-',
-        },
-        'MarisaA': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': 'th7_ud0002.rpy',
-          'detail': '-',
-        },
-        'MarisaB': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': null,
-          'detail': '-',
-        },
-        'SakuyaA': {
-          'score': 13000000,
-          'status': 'good',
-          'date': '2024-03-11T15:30:00',
-          'replay': 'th7_ud0002.rpy',
           'detail': '-',
         },
         'SakuyaB': {
