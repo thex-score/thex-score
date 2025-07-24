4a8aebaf8b149de515813d9103d062ebeff29db3
nyankoth11 <nyankoshiroan@gmail.com>
2025-07-24 19:13:05 +0900

    add-page-data

---
+++ pages/data.vue
@@ -0,0 +1,90 @@
+<template>
+  <UContainer class="py-8">
+    <h1 class="text-xl font-bold mb-6">各種データ</h1>
+
+    <UCard class="my-5">
+      <template #header>
+        <p>作品・部門別 達成状況</p>
+      </template>
+      <table>
+        <tr>
+          <th align="center">作品</th>
+          <th align="center">超大台</th>
+          <th align="center">大台</th>
+        </tr>
+        <tr>
+          <td>th06 東方紅魔郷 EoSD</td>
+          <td align="right">29</td>
+          <td align="right">112</td>
+        </tr>
+        <tr>
+          <td>th07 東方妖々夢 PCB Ex</td>
+          <td align="right">7</td>
+          <td align="right">55</td>
+        </tr>
+        <tr>
+          <td>th07 東方妖々夢 PCB Ph</td>
+          <td align="right">4</td>
+          <td align="right">69</td>
+        </tr>
+        <tr>
+          <td>th08 東方永夜抄 IN</td>
+          <td align="right">24</td>
+          <td align="right">98</td>
+        </tr>
+        <tr>
+          <td>th10 東方風神録 MoF</td>
+          <td align="right">9</td>
+          <td align="right">155</td>
+        </tr>
+        <tr>
+          <td>th11 東方地霊殿 SA</td>
+          <td align="right">21</td>
+          <td align="right">162</td>
+        </tr>
+        <tr>
+          <td>th12 東方星蓮船 UFO</td>
+          <td align="right">16</td>
+          <td align="right">133</td>
+        </tr>
+        <tr>
+          <td>th12.8 妖精大戦争 GFW</td>
+          <td align="right">3</td>
+          <td align="right">15</td>
+        </tr>
+        <tr>
+          <td>th13 東方神霊廟 TD</td>
+          <td align="right">12</td>
+          <td align="right">46</td>
+        </tr>
+        <tr>
+          <td>th14 東方輝針城 DDC</td>
+          <td align="right">16</td>
+          <td align="right">61</td>
+        </tr>
+        <tr>
+          <td>th15 東方紺珠伝 LoLK</td>
+          <td align="right">10</td>
+          <td align="right">25</td>
+        </tr>
+        <tr>
+          <td>th16 東方天空璋 HSiFS</td>
+          <td align="right">21</td>
+          <td align="right">49</td>
+        </tr>
+        <tr>
+          <td>th17 東方鬼形獣 WBaWC</td>
+          <td align="right">9</td>
+          <td align="right">33</td>
+        </tr>
+        <tr>
+          <td>th18 東方虹龍洞 UM</td>
+          <td align="right">14</td>
+          <td align="right">26</td>
+        </tr>
+      </table>
+    </UCard>
+
+
+</UContainer>
+</template>
