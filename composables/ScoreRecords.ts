445539ba462b08211c4fbaf776fc6374ecf07705
nyankoth11 <nyankoshiroan@gmail.com>
2025-08-13 17:09:47 +0900

    init-record-OSCAR-th15-reisen

--- composables/ScoreRecords.ts
+++ composables/ScoreRecords.ts
@@ -4170,6 +4170,17 @@ export function useScoreRecords(){
         },
       },
     },
+    'Oscar': {
+      'th15': {
+        'Reisen': {
+          'score': 1017435330,
+          'status': 'great',
+          'date': '2025-08-11T15:30:00',
+          'replay': 'th15_ud0026.rpy',
+          'detail': '-',
+        },
+      },
+    },
     'OYM': {
       'th11': {
         'ReimuA': {
