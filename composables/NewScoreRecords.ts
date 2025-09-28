03062e820b04550a0545994249f127957da23079
nyankoth11 <nyankoshiroan@gmail.com>
2025-09-29 00:29:46 +0900

    feature/modify-record-Altair-th11-reimuA-108167

--- composables/NewScoreRecords.ts
+++ composables/NewScoreRecords.ts
@@ -2,6 +2,16 @@
 export function useNewScoreRecords() {
   const newScoreRecords = [
     {
+      player: "Altair",
+      game: "th11",
+      shot_type: "ReimuA",
+      score: 1081673800,
+      status: "good",
+      date: "2025-09-28T15:30:00",
+      replay: "th11_ud0154.rpy",
+      detail: "-",
+    },
+    {
       player: "TRDario",
       game: "th11",
       shot_type: "ReimuA",
