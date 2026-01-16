0818397a4e204d9ab9b0247a7f48e6ad90db1453
nyankoth11 <nyankoshiroan@gmail.com>
2026-01-17 02:38:25 +0900

    feature/modify-record-kasodanimoko-th20-reimuR2-72711

--- composables/NewScoreRecords.ts
+++ composables/NewScoreRecords.ts
@@ -4,6 +4,16 @@ export function useNewScoreRecords() {
     {
       player: "幽谷もこ",
       game: "th20",
+      shot_type: "ReimuR2",
+      score: 727113980,
+      status: "great",
+      date: "2026-01-17T15:30:00",
+      replay: "th20_ud0023.rpy",
+      detail: "-",
+    },
+    {
+      player: "幽谷もこ",
+      game: "th20",
       shot_type: "ReimuR1",
       score: 702547600,
       status: "great",
